1b127817824c2b931f7e1f14fac6b070871ae59c
Mikey McCormack <mccormack@berkeley.edu>
2016-04-24 09:43:16 -0700

    Clean up cost and revenue assumptions service

--- app/assets/javascripts/angular_app/services/cost_and_revenue_assumptions_service.js
+++ app/assets/javascripts/angular_app/services/cost_and_revenue_assumptions_service.js
@@ -15,25 +15,31 @@ angular.module('rentals').service('cost_and_revenue_assumptions_service', functi
   /* --- Private --- */
 
   function get_avg_rent_per_sq_ft(property) {
-    var assumptions = property.financing_and_income_assumption;
+    var total_area = property.financing_and_income_assumption.total_square_feet;
     var gross_rent = get_gross_monthly_rent(property);
-    return gross_rent / assumptions.total_square_feet;
+
+    return gross_rent / total_area;
   }
 
   function get_total_cost_per_sq_ft(property) {
-    var assumptions = property.financing_and_income_assumption;
+    var total_area = property.financing_and_income_assumption.total_square_feet;
     var total_cost = get_total_cost(property);
-    return total_cost / assumptions.total_square_feet;
+
+    return total_cost / total_area;
   }
 
   function get_avg_area_per_unit(property) {
-    var assumptions = property.financing_and_income_assumption;
-    return assumptions.total_square_feet / assumptions.number_of_units;
+    var number_of_units = property.financing_and_income_assumption.number_of_units;
+    var total_area = property.financing_and_income_assumption.total_square_feet;
+
+    return total_area / number_of_units;
   }
 
   function get_cost_per_unit(property) {
     var total_cost = get_total_cost(property);
-    return total_cost / property.financing_and_income_assumption.number_of_units;
+    var number_of_units = property.financing_and_income_assumption.number_of_units;
+
+    return total_cost / number_of_units;
   }
 
   function get_loan_origination_fee(property) {
@@ -44,52 +50,55 @@ angular.module('rentals').service('cost_and_revenue_assumptions_service', functi
   }
 
   function get_closing_costs(property) {
-    if(!property) {
-      return 0;
+    function clean_up(costs) {
+      var costs_copy = angular.copy(costs);
+      costs_copy.rental_property_id = 0;
+      costs_copy.id = 0;
+      return costs_copy;
     }
 
-    var costs_copy = angular.copy(property.closing_cost);
-    costs_copy.id = 0;
-    costs_copy.rental_property_id = 0;
+    function tally(costs) {
+      var total_costs = 0;
+      _.each(_.values(costs), function(cost) {
+        total_costs += parseFloat(cost);
+      });
+      return total_costs;
+    }
 
-    var total_costs = 0;
-    _.each(_.values(costs_copy), function(cost) {
-      total_costs += parseFloat(cost);
-    });
-    return total_costs;
+    return tally(clean_up(property.closing_cost));
   }
 
   function get_gross_monthly_rent(property) {
-    var assumptions = property.financing_and_income_assumption;
-    return assumptions.number_of_units * assumptions.average_monthly_rent_per_unit;
+    var number_of_units = property.financing_and_income_assumption.number_of_units;
+    var avg_rent_per_unit = property.financing_and_income_assumption.average_monthly_rent_per_unit;
+
+    return number_of_units * avg_rent_per_unit;
   }
 
   function get_total_gross_monthly_income(property) {
-    var assumptions = property.financing_and_income_assumption;
+    var other_income = parseFloat(property.financing_and_income_assumption.other_monthly_income);
     var gross_monthly_rent = get_gross_monthly_rent(property);
-    return parseFloat(gross_monthly_rent) +
-      parseFloat(assumptions.other_monthly_income);
+
+    return gross_monthly_rent + other_income;
   }
 
   function get_total_cost(property) {
-    var financing_and_income_assumptions = property.financing_and_income_assumption;
     var closing_costs = get_closing_costs(property);
+    var land_cost = parseFloat(property.financing_and_income_assumption.land_cost);
+    var building_cost = parseFloat(property.financing_and_income_assumption.building_cost);
+    var improvements = parseFloat(property.financing_and_income_assumption.improvements);
 
-    return parseFloat(closing_costs) +
-      parseFloat(financing_and_income_assumptions.land_cost) +
-      parseFloat(financing_and_income_assumptions.building_cost) +
-      parseFloat(financing_and_income_assumptions.improvements);
+    return closing_costs + land_cost + building_cost + improvements;
   }
 
   function get_projected_average_rents(property) {
     var rent_increases = property.income_and_cost_projection.rent_increases;
     var rent = property.financing_and_income_assumption.average_monthly_rent_per_unit;
-    var average_rents = [];
-    _.each(rent_increases, function(increase) {
+
+    return _.map(rent_increases, function(increase) {
       var new_rent = rent * (1 + (increase / 100));
       rent = new_rent;
-      average_rents.push(new_rent);
+      return new_rent;
     });
-    return average_rents;
   }
 });
--- app/assets/javascripts/angular_app/spec/services/cost_and_revenue_assumptions_service_spec.js
+++ app/assets/javascripts/angular_app/spec/services/cost_and_revenue_assumptions_service_spec.js
@@ -6,32 +6,22 @@ describe('cost_and_revenue_assumptions_service', function() {
     subject = this.cost_and_revenue_assumptions_service;
   });
 
-  describe('when closing costs do not exist', function() {
-    it('returns zero', function() {
-      var val = subject.get_closing_costs();
-      expect(val).toEqual(0);
-    });
+  it('returns the sum of costs', function() {
+    var val = subject.get_closing_costs({closing_cost: {a: 1, b: 2}});
+    expect(val).toEqual(3);
   });
 
-  describe('when closing costs exist', function() {
-    it('returns zero', function() {
-      var val = subject.get_closing_costs({closing_cost: {a: 1, b: 2}});
-      expect(val).toEqual(3);
-    });
-
-    it('ignores id and rental property ids', function() {
-      var val = subject.get_closing_costs({closing_cost: {a: 1, b: 2, id: 17, rental_property_id: 44}});
-      expect(val).toEqual(3);
-    });
-
-    it('does not change the input value', function() {
-      var original = {closing_cost: {a: 1, b: 2, id: 17, rental_property_id: 44}};
-      var copy = angular.copy(original);
-
-      subject.get_closing_costs(original);
-
-      expect(copy).toEqual(original);
-    });
+  it('ignores id and rental property ids', function() {
+    var val = subject.get_closing_costs({closing_cost: {a: 1, b: 2, id: 17, rental_property_id: 44}});
+    expect(val).toEqual(3);
   });
 
+  it('does not change the input value', function() {
+    var original = {closing_cost: {a: 1, b: 2, id: 17, rental_property_id: 44}};
+    var copy = angular.copy(original);
+
+    subject.get_closing_costs(original);
+
+    expect(copy).toEqual(original);
+  });
 });
