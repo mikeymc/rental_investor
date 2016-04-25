a15d5f8b1685cf70251103fa70de5ebfbc7ef8c6
Mikey McCormack <mccormack@berkeley.edu>
2016-04-24 17:48:17 -0700

    Move project functions from the operating revenues directive into the property service

--- app/assets/javascripts/angular_app/directives/operating_revenues.js
+++ app/assets/javascripts/angular_app/directives/operating_revenues.js
@@ -8,82 +8,16 @@ angular.module('rentals').directive('operatingRevenues', function(property_servi
           return;
         }
         $scope.monthly_gross_scheduled_rent_income = property_service.get_gross_monthly_rent($scope.rental_property);
-        $scope.vacancy = vacancy($scope.rental_property);
-        $scope.net_rental_income = net_rental_income($scope.rental_property);
+        $scope.vacancy = property_service.get_vacancy_operating_expense($scope.rental_property);
+        $scope.net_rental_income = property_service.get_net_rental_income($scope.rental_property);
         $scope.other_income = $scope.rental_property.financing_and_income_assumption.other_monthly_income;
-        $scope.gross_income = gross_income($scope.rental_property);
-        $scope.projected_gross_annual_rents = projected_gross_annual_rents($scope.rental_property);
-        $scope.projected_annual_vacancy_costs = projected_annual_vacancy_costs($scope.rental_property);
-        $scope.projected_annual_net_rental_incomes = projected_annual_net_rental_incomes($scope.rental_property);
-        $scope.projected_other_incomes = projected_other_incomes($scope.rental_property);
-        $scope.projected_gross_incomes = projected_gross_incomes($scope.rental_property);
+        $scope.gross_income = property_service.get_gross_operating_income($scope.rental_property);
+        $scope.projected_gross_annual_rents = property_service.get_projected_gross_annual_rents($scope.rental_property);
+        $scope.projected_annual_vacancy_costs = property_service.get_projected_annual_vacancy_costs($scope.rental_property);
+        $scope.projected_annual_net_rental_incomes = property_service.get_projected_annual_net_rental_incomes($scope.rental_property);
+        $scope.projected_other_incomes = property_service.get_projected_other_incomes($scope.rental_property);
+        $scope.projected_gross_incomes = property_service.get_projected_annual_gross_operating_incomes($scope.rental_property);
       }, true);
-
-      /* --- Private --- */
-
-      function gross_income(property) {
-        var net_income = net_rental_income(property);
-        var other_income = property.financing_and_income_assumption.other_monthly_income;
-        return net_income + parseFloat(other_income)
-      }
-
-      function vacancy(property) {
-        var vacancy_rate = property.operating_expenses_assumption.vacancy_rate;
-        var gross_rent = property_service.get_gross_monthly_rent(property);
-        return gross_rent * vacancy_rate / 100;
-      }
-
-      function net_rental_income(property) {
-        var gross_rent = property_service.get_gross_monthly_rent(property);
-        var vacancy_cost = vacancy(property);
-        return gross_rent - vacancy_cost;
-      }
-
-      function projected_gross_incomes(property) {
-        var projected_nets = projected_annual_net_rental_incomes(property);
-        var projected_others = projected_other_incomes(property);
-
-        return _.map(projected_nets, function(net, index) {
-          return net + projected_others[index];
-        });
-      }
-
-      function projected_other_incomes(property) {
-        var rent_increases = property.income_and_cost_projection.rent_increases;
-        var income = 12 * property.financing_and_income_assumption.other_monthly_income;
-
-        return _.map(rent_increases, function(increase) {
-          income = income * (1 + increase / 100);
-          return income;
-        });
-      }
-
-      function projected_annual_net_rental_incomes(property) {
-        var gross_incomes = projected_gross_annual_rents(property);
-        var vacancy_costs = projected_annual_vacancy_costs(property);
-
-        return _.map(gross_incomes, function(income, index) {
-          return income - vacancy_costs[index];
-        });
-      }
-
-      function projected_annual_vacancy_costs(property) {
-        var rents = projected_gross_annual_rents(property);
-        var vacancy_rate = property.operating_expenses_assumption.vacancy_rate / 100;
-
-        return _.map(rents, function(rent) {
-          return rent * vacancy_rate;
-        })
-      }
-
-      function projected_gross_annual_rents(property) {
-        var projected_average_monthly_rents = property_service.get_projected_average_rents(property);
-        var number_of_units = property.financing_and_income_assumption.number_of_units;
-
-        return _.map(projected_average_monthly_rents, function(rent) {
-          return rent * 12 * number_of_units;
-        })
-      }
     }
   }
 });
--- app/assets/javascripts/angular_app/services/property_service.js
+++ app/assets/javascripts/angular_app/services/property_service.js
@@ -9,10 +9,72 @@ angular.module('rentals').service('property_service', function() {
     get_cost_per_unit: get_cost_per_unit,
     get_avg_area_per_unit: get_avg_area_per_unit,
     get_total_cost_per_sq_ft: get_total_cost_per_sq_ft,
-    get_avg_rent_per_sq_ft: get_avg_rent_per_sq_ft
+    get_avg_rent_per_sq_ft: get_avg_rent_per_sq_ft,
+    get_projected_other_incomes: get_projected_other_incomes,
+    get_vacancy_operating_expense: get_vacancy_operating_expense,
+    get_net_rental_income: get_net_rental_income,
+    get_gross_operating_income: get_gross_operating_income,
+    get_projected_gross_annual_rents: get_projected_gross_annual_rents,
+    get_projected_annual_vacancy_costs: get_projected_annual_vacancy_costs,
+    get_projected_annual_net_rental_incomes: get_projected_annual_net_rental_incomes,
+    get_projected_annual_gross_operating_incomes: get_projected_annual_gross_operating_incomes
   };
 
   /* --- Private --- */
+
+  function get_projected_annual_gross_operating_incomes(property) {
+    var projected_nets = get_projected_annual_net_rental_incomes(property);
+    var projected_others = get_projected_other_incomes(property);
+
+    return _.map(projected_nets, function(net, index) {
+      return net + projected_others[index];
+    });
+  }
+
+  function get_projected_annual_net_rental_incomes(property) {
+    var gross_incomes = get_projected_gross_annual_rents(property);
+    var vacancy_costs = get_projected_annual_vacancy_costs(property);
+
+    return _.map(gross_incomes, function(income, index) {
+      return income - vacancy_costs[index];
+    });
+  }
+
+  function get_projected_annual_vacancy_costs(property) {
+    var rents = get_projected_gross_annual_rents(property);
+    var vacancy_rate = property.operating_expenses_assumption.vacancy_rate / 100;
+
+    return _.map(rents, function(rent) {
+      return rent * vacancy_rate;
+    })
+  }
+
+  function get_projected_gross_annual_rents(property) {
+    var projected_average_monthly_rents = get_projected_average_rents(property);
+    var number_of_units = property.financing_and_income_assumption.number_of_units;
+
+    return _.map(projected_average_monthly_rents, function(rent) {
+      return rent * 12 * number_of_units;
+    })
+  }
+
+  function get_gross_operating_income(property) {
+    var net_income = get_net_rental_income(property);
+    var other_income = property.financing_and_income_assumption.other_monthly_income;
+    return net_income + parseFloat(other_income)
+  }
+
+  function get_net_rental_income(property) {
+    var gross_rent = get_gross_monthly_rent(property);
+    var vacancy_cost = get_vacancy_operating_expense(property);
+    return gross_rent - vacancy_cost;
+  }
+
+  function get_vacancy_operating_expense(property) {
+    var vacancy_rate = property.operating_expenses_assumption.vacancy_rate;
+    var gross_rent = get_gross_monthly_rent(property);
+    return gross_rent * vacancy_rate / 100;
+  }
 
   function get_avg_rent_per_sq_ft(property) {
     var total_area = property.financing_and_income_assumption.total_square_feet;
@@ -99,6 +161,16 @@ angular.module('rentals').service('property_service', function() {
       var new_rent = rent * (1 + (increase / 100));
       rent = new_rent;
       return new_rent;
+    });
+  }
+
+  function get_projected_other_incomes(property) {
+    var rent_increases = property.income_and_cost_projection.rent_increases;
+    var income = 12 * property.financing_and_income_assumption.other_monthly_income;
+
+    return _.map(rent_increases, function(increase) {
+      income = income * (1 + increase / 100);
+      return income;
     });
   }
 });
