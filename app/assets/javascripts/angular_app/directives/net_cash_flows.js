e53d059dda553edb275b2e494bfb409ab0b27677
Mikey McCormack <mccormack@berkeley.edu>
2016-04-30 11:25:10 -0700

    Add five year cash flows

--- app/assets/javascripts/angular_app/directives/net_cash_flows.js
+++ app/assets/javascripts/angular_app/directives/net_cash_flows.js
@@ -11,6 +11,7 @@ angular.module('rentals').directive('netCashFlows', function(property_service, c
         $scope.monthly_cash_flow = -1 * property_service.down_payment($scope.rental_property);
         $scope.one_year_exit_net = one_year_exit_net($scope.rental_property);
         $scope.three_year_exit_nets = three_year_exit_nets($scope.rental_property);
+        $scope.five_year_exit_nets = five_year_exit_nets($scope.rental_property);
 
         /* --- Private --- */
 
@@ -44,6 +45,36 @@ angular.module('rentals').directive('netCashFlows', function(property_service, c
           cfs.push(annual_remaining_cash_flows[0]);
           cfs.push(annual_remaining_cash_flows[1]);
           cfs.push(three_year_exit_gain(property));
+
+          return cfs;
+        }
+
+        function five_year_exit_nets(property) {
+          function five_year_exit_gain(property) {
+            var annual_principal_reductions = cash_flow_service.yearly_cum_princ(property);
+
+            var values = [
+              cash_flow_service.get_annual_total_returns(property)[4],
+              annual_principal_reductions[0],
+              annual_principal_reductions[1],
+              annual_principal_reductions[2],
+              annual_principal_reductions[3],
+              property_service.down_payment(property),
+              exit_scenarios_service.fifth_year_gain_on_sale(property)
+            ];
+
+            return _.reduce(values, function(memo, item) {
+              return memo + parseFloat(item);
+            }, 0);
+          }
+
+          var cfs = [];
+          var annual_remaining_cash_flows = cash_flow_service.annual_cash_flows_remaining(property);
+          cfs.push(annual_remaining_cash_flows[0]);
+          cfs.push(annual_remaining_cash_flows[1]);
+          cfs.push(annual_remaining_cash_flows[2]);
+          cfs.push(annual_remaining_cash_flows[3]);
+          cfs.push(five_year_exit_gain(property));
 
           return cfs;
         }
--- spec/journeys/helpers/expectations.rb
+++ spec/journeys/helpers/expectations.rb
@@ -425,6 +425,7 @@ class Expectations
         total_roi: %w(21.31% 25.57% 29.97% 32.93% 35.78%),
         one_year_exit_net_cfs: %w(-$515,681 $625,570),
         three_year_exit_net_cfs: %w(-$515,681 $63,374 $82,605 $1,233,174),
+        five_year_exit_net_cfs: %w(-$515,681 $63,374 $82,605 $102,405 $114,563 $1,652,837),
         one_year_exit_price_gain: %w($3,033,420 $0 8.36%),
         three_year_exit_price_gain: %w($3,500,565 $467,145 8.36%),
         five_year_exit_price_gain: %w($3,782,880 $749,460 8.36%)
@@ -436,6 +437,7 @@ class Expectations
         total_roi: %w(18.69% 20.99% 23.42% 25.18% 26.89%),
         one_year_exit_net_cfs: %w(-$61,082 $72,499),
         three_year_exit_net_cfs: %w(-$61,082 $7,115 $8,341 $120,781),
+        five_year_exit_net_cfs: %w(-$61,082 $7,115 $8,341 $9,646 $10,527 $157,503),
         one_year_exit_price_gain: %w($305,410 $0 6.91%),
         three_year_exit_price_gain: %w($342,023 $36,613 6.91%),
         five_year_exit_price_gain: %w($367,112 $61,702 6.91%)
@@ -463,9 +465,11 @@ class Expectations
     details[:one_year_exit_net_cfs].each do |item|
       expect(cfs.find('.row', text: 'Net CFs from Investment - 1 Yr Exit')).to have_content item
     end
-
     details[:three_year_exit_net_cfs].each do |item|
       expect(cfs.find('.row', text: 'Net CFs from Investment - 3 Yr Exit')).to have_content item
+    end
+    details[:five_year_exit_net_cfs].each do |item|
+      expect(cfs.find('.row', text: 'Net CFs from Investment - 5 Yr Exit')).to have_content item
     end
 
     exits = page.find('#exit-scenarios')
