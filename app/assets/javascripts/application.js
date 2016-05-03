e70c9995d2c9fac1800453ac38fca4d385a8fd2a
Mikey McCormack <mccormack@berkeley.edu>
2016-05-02 19:54:58 -0700

    Remove turbolinks to make angular ui-router back button work well.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,6 @@
 //= require angular/angular.js
 //= require angular-ui-router/release/angular-ui-router.js
 //= require underscore/underscore.js
-//= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree ./angular_app/modules
 //= require_tree ./angular_app/controllers
