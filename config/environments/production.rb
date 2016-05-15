edd19b5683175b56e6aaa4aa5d47745b0c60044e
Mikey McCormack <mccormack@berkeley.edu>
2016-05-15 11:29:17 -0700

    Add `Rails.application.routes.default_url_options[:host] = 'domain.com'` to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,3 +62,5 @@ Rentals::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 end
+
+Rails.application.routes.default_url_options[:host] = 'domain.com'
