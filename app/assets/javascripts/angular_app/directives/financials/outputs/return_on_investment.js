70afc926ff496e6561ce6c56f3b0e22f6cf61fa8
Mikey McCormack <mccormack@berkeley.edu>
2016-10-04 11:01:10 -0700

    Move ROI calculations into an ROI service

--- app/assets/javascripts/angular_app/directives/financials/outputs/return_on_investment.js
+++ app/assets/javascripts/angular_app/directives/financials/outputs/return_on_investment.js
@@ -1,4 +1,4 @@
-angular.module('rentals').directive('returnOnInvestment', function(cash_flow_service, operating_expenses_service, property_service, noi_service) {
+angular.module('rentals').directive('returnOnInvestment', function(roi_service) {
   return {
     templateUrl: 'investment_properties_pages/financials/outputs/return_on_investment.html',
     restrict: 'E',
@@ -8,43 +8,11 @@ angular.module('rentals').directive('returnOnInvestment', function(cash_flow_ser
           return;
         }
 
-        $scope.noi_roi = annual_noi_roi($scope.rental_property);
-        $scope.cash_roi = cash_roi($scope.rental_property);
-        $scope.total_roi = total_roi($scope.rental_property);
+        $scope.noi_roi = roi_service.annual_noi_roi($scope.rental_property);
+        $scope.cash_roi = roi_service.cash_roi($scope.rental_property);
+        $scope.total_roi = roi_service.total_roi($scope.rental_property);
+
       }, true);
-
-      /* --- Private --- */
-
-      function annual_noi_roi(property) {
-        var down_payment = property_service.down_payment(property);
-        var operating_expenses = operating_expenses_service.all_operating_expenses(property).total.yearly_costs;
-        var gross_income = property_service.get_projected_annual_gross_operating_incomes(property);
-        var annual_interest_on_loan = noi_service.get_annual_interest_on_loan(property);
-        var depreciation = noi_service.get_annual_building_depreciation(property);
-
-        return _.map(gross_income, function(income, index) {
-          return 100 * (income - operating_expenses[index] - annual_interest_on_loan[index] - depreciation) / down_payment;
-        });
-      }
-
-      function cash_roi(property) {
-        var remaining_cf = cash_flow_service.annual_cash_flows_remaining(property);
-        var down_payment = property_service.down_payment(property);
-
-        return _.map(remaining_cf, function(cf) {
-          return 100 * cf / down_payment;
-        });
-      }
-
-      function total_roi(property) {
-        var remaining_cf = cash_flow_service.annual_cash_flows_remaining(property);
-        var yearly_cum_princ = cash_flow_service.yearly_cum_princ(property);
-        var down_payment = property_service.down_payment(property);
-
-        return _.map(remaining_cf, function(cf, index) {
-          return 100 * (cf + yearly_cum_princ[index]) / down_payment;
-        });
-      }
     }
   }
 });
---
+++ app/assets/javascripts/angular_app/services/roi_service.js
@@ -0,0 +1,40 @@
+angular.module('rentals').service('roi_service', function(property_service, operating_expenses_service, noi_service, cash_flow_service) {
+  return {
+    annual_noi_roi: annual_noi_roi,
+    cash_roi: cash_roi,
+    total_roi: total_roi
+  };
+
+  /* --- Private --- */
+
+  function annual_noi_roi(property) {
+    var down_payment = property_service.down_payment(property);
+    var operating_expenses_each_year = operating_expenses_service.all_operating_expenses(property).total.yearly_costs;
+    var gross_incomes_each_year = property_service.get_projected_annual_gross_operating_incomes(property);
+    var interest_on_loan_each_year = noi_service.get_annual_interest_on_loan(property);
+    var depreciation_each_year = noi_service.get_annual_building_depreciation(property);
+
+    return _.map(gross_incomes_each_year, function(gross_income_for_the_year, year) {
+      return 100 * (gross_income_for_the_year - operating_expenses_each_year[year] - interest_on_loan_each_year[year] - depreciation_each_year) / down_payment;
+    });
+  }
+
+  function cash_roi(property) {
+    var remaining_cash_flow_each_year = cash_flow_service.annual_cash_flows_remaining(property);
+    var down_payment_on_the_property = property_service.down_payment(property);
+
+    return _.map(remaining_cash_flow_each_year, function(cash_flow_that_year) {
+      return 100 * cash_flow_that_year / down_payment_on_the_property;
+    });
+  }
+
+  function total_roi(property) {
+    var down_payment = property_service.down_payment(property);
+    var remaining_annual_cash_flows = cash_flow_service.annual_cash_flows_remaining(property);
+    var yearly_cumulative_payment_on_the_principal = cash_flow_service.yearly_cum_princ(property);
+
+    return _.map(remaining_annual_cash_flows, function(remaining_cash_flow_this_year, year) {
+      return 100 * (remaining_cash_flow_this_year + yearly_cumulative_payment_on_the_principal[year]) / down_payment;
+    });
+  }
+});
