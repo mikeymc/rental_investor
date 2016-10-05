6fdb5959cc694ec00fee0f4c21a16bbe7e6bdd8a
Mikey McCormack <mccormack@berkeley.edu>
2016-10-05 12:10:09 -0700

    Make exitScenariosService internals camelCase

--- app/assets/javascripts/angular_app/services/exitScenariosService.js
+++ app/assets/javascripts/angular_app/services/exitScenariosService.js
@@ -1,57 +1,57 @@
 angular.module('rentals').service('exit_scenarios_service', function(key_rent_ratios_service, noi_service, operating_expenses_service, propertyService) {
   return {
-    first_year_exit_price: first_year_exit_price,
-    third_year_exit_price: third_year_exit_price,
-    fifth_year_exit_price: fifth_year_exit_price,
-    first_year_gain_on_sale: first_year_gain_on_sale,
-    third_year_gain_on_sale: third_year_gain_on_sale,
-    fifth_year_gain_on_sale: fifth_year_gain_on_sale
+    first_year_exit_price: firstYearExitPrice,
+    third_year_exit_price: thirdYearExitPrice,
+    fifth_year_exit_price: fifthYearExitPrice,
+    first_year_gain_on_sale: firstYearGainOnSale,
+    third_year_gain_on_sale: thirdYearGainOnSale,
+    fifth_year_gain_on_sale: fifthYearGainOnSale
   };
 
   /* --- Private --- */
 
-  function first_year_exit_price(property) {
-    var cap_rate = key_rent_ratios_service.get_cap_rate(property);
+  function firstYearExitPrice(property) {
+    var capRate = key_rent_ratios_service.get_cap_rate(property);
     var expenses = operating_expenses_service.all_operating_expenses(property);
-    var noi = noi_service.net_annual_incomes(property, expenses)[0];
+    var netOperatingIncome = noi_service.net_annual_incomes(property, expenses)[0];
 
-    return noi / (0.01 * cap_rate);
+    return netOperatingIncome / (0.01 * capRate);
   }
 
-  function third_year_exit_price(property) {
-    var cap_rate = key_rent_ratios_service.get_cap_rate(property);
+  function thirdYearExitPrice(property) {
+    var capRate = key_rent_ratios_service.get_cap_rate(property);
     var expenses = operating_expenses_service.all_operating_expenses(property);
-    var noi = noi_service.net_annual_incomes(property, expenses)[2];
+    var netOperatingIncome = noi_service.net_annual_incomes(property, expenses)[2];
 
-    return noi / (0.01 * cap_rate);
+    return netOperatingIncome / (0.01 * capRate);
   }
 
-  function fifth_year_exit_price(property) {
-    var cap_rate = key_rent_ratios_service.get_cap_rate(property);
+  function fifthYearExitPrice(property) {
+    var capRate = key_rent_ratios_service.get_cap_rate(property);
     var expenses = operating_expenses_service.all_operating_expenses(property);
-    var noi = noi_service.net_annual_incomes(property, expenses)[4];
+    var netOperatingIncome = noi_service.net_annual_incomes(property, expenses)[4];
 
-    return noi / (0.01 * cap_rate);
+    return netOperatingIncome / (0.01 * capRate);
   }
 
-  function first_year_gain_on_sale(property) {
-    var price = first_year_exit_price(property);
-    var cost = propertyService.getTotalCost(property);
+  function firstYearGainOnSale(property) {
+    var exitPrice = firstYearExitPrice(property);
+    var initialCost = propertyService.getTotalCost(property);
 
-    return price - cost;
+    return exitPrice - initialCost;
   }
 
-  function third_year_gain_on_sale(property) {
-    var price = third_year_exit_price(property);
-    var cost = propertyService.getTotalCost(property);
+  function thirdYearGainOnSale(property) {
+    var exitPrice = thirdYearExitPrice(property);
+    var initialCost = propertyService.getTotalCost(property);
 
-    return price - cost;
+    return exitPrice - initialCost;
   }
 
-  function fifth_year_gain_on_sale(property) {
-    var price = fifth_year_exit_price(property);
-    var cost = propertyService.getTotalCost(property);
+  function fifthYearGainOnSale(property) {
+    var exitPrice = fifthYearExitPrice(property);
+    var initialCost = propertyService.getTotalCost(property);
 
-    return price - cost;
+    return exitPrice - initialCost;
   }
 });
