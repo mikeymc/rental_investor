70ffe21267f9b5f4400d64c360b93c7c36c115f0
Mikey McCormack <mccormack@berkeley.edu>
2016-10-05 12:43:44 -0700

    Camel case the internals of the noiService

--- app/assets/javascripts/angular_app/services/noiService.js
+++ app/assets/javascripts/angular_app/services/noiService.js
@@ -1,104 +1,104 @@
 angular.module('rentals').service('noiService', function(propertyService, cashFlowService) {
   return {
-    monthly_net_income_before_taxes: monthly_net_income_before_taxes,
-    monthly_net_income_after_taxes: monthly_net_income_after_taxes,
-    annual_net_income_before_taxes: annual_net_income_before_taxes,
-    annual_net_income_after_taxes: annual_net_income_after_taxes,
-    get_annual_building_depreciation: get_annual_building_depreciation,
-    get_monthly_building_depreciation: get_monthly_building_depreciation,
-    get_annual_interest_on_loan: get_annual_interest_on_loan,
-    monthly_interest_on_loan_percentage: monthly_interest_on_loan_percentage,
-    monthly_interest_on_loan: monthly_interest_on_loan,
-    net_monthly_income: net_monthly_income,
-    net_annual_incomes: net_annual_incomes,
-    annual_taxes: annual_taxes,
-    monthly_taxes: monthly_taxes
+    monthly_net_income_before_taxes: getMonthlyNetIncomeBeforeTaxes,
+    monthly_net_income_after_taxes: getMonthlyNetIncomeAfterTaxes,
+    annual_net_income_before_taxes: getAnnualNetIncomeBeforeTaxes,
+    annual_net_income_after_taxes: getAnnualNetIncomeAfterTaxes,
+    get_annual_building_depreciation: getAnnualBuildingDepreciation,
+    get_monthly_building_depreciation: getMonthlyBuildingDepreciation,
+    get_annual_interest_on_loan: getAnnualInterestOnLoan,
+    monthly_interest_on_loan_percentage: getMonthlyInterestOnLoanPercentage,
+    monthly_interest_on_loan: getMonthlyInterestOnLoan,
+    net_monthly_income: getNetMonthlyIncome,
+    net_annual_incomes: getNetAnnualIncomes,
+    annual_taxes: getAnnualTaxes,
+    monthly_taxes: getMonthlyTaxes
   };
 
-  function monthly_net_income_before_taxes(property, expenses) {
-    var income = net_monthly_income(property, expenses);
-    var interest = monthly_interest_on_loan(property);
-    var depreciation = get_monthly_building_depreciation(property);
+  function getMonthlyNetIncomeBeforeTaxes(property, expenses) {
+    var income = getNetMonthlyIncome(property, expenses);
+    var interest = getMonthlyInterestOnLoan(property);
+    var depreciation = getMonthlyBuildingDepreciation(property);
     return income - interest - depreciation;
   }
 
-  function monthly_net_income_after_taxes(property, expenses) {
-    var before_taxes = monthly_net_income_before_taxes(property, expenses);
-    var taxes = monthly_taxes(property, expenses);
+  function getMonthlyNetIncomeAfterTaxes(property, expenses) {
+    var monthlyNetIncomeBeforeTaxes = getMonthlyNetIncomeBeforeTaxes(property, expenses);
+    var monthlyTaxes = getMonthlyTaxes(property, expenses);
 
-    return before_taxes - taxes;
+    return monthlyNetIncomeBeforeTaxes - monthlyTaxes;
   }
 
-  function annual_net_income_before_taxes(property, expenses) {
-    var incomes = net_annual_incomes(property, expenses);
-    var depreciation = get_annual_building_depreciation(property);
-    var interest = get_annual_interest_on_loan(property);
+  function getAnnualNetIncomeBeforeTaxes(property, expenses) {
+    var netAnnualIncomes = getNetAnnualIncomes(property, expenses);
+    var annualBuildingDepreciation = getAnnualBuildingDepreciation(property);
+    var annualInterestOnLoan = getAnnualInterestOnLoan(property);
 
-    return _.map(incomes, function(income, index) {
-      return income - depreciation - interest[index];
+    return _.map(netAnnualIncomes, function(incomeForTheYear, year) {
+      return incomeForTheYear - annualBuildingDepreciation - annualInterestOnLoan[year];
     })
   }
 
-  function annual_net_income_after_taxes(property, expenses) {
-    var incomes = annual_net_income_before_taxes(property, expenses);
-    var taxes = annual_taxes(property, expenses);
+  function getAnnualNetIncomeAfterTaxes(property, expenses) {
+    var annualNetIncomeBeforeTaxes = getAnnualNetIncomeBeforeTaxes(property, expenses);
+    var annualTaxes = getAnnualTaxes(property, expenses);
 
-    return _.map(incomes, function(income, index) {
-      return income - taxes[index];
+    return _.map(annualNetIncomeBeforeTaxes, function(income, year) {
+      return income - annualTaxes[year];
     })
   }
 
-  function get_annual_building_depreciation(property) {
-    return 12 * get_monthly_building_depreciation(property);
+  function getAnnualBuildingDepreciation(property) {
+    return 12 * getMonthlyBuildingDepreciation(property);
   }
 
-  function get_monthly_building_depreciation(property) {
+  function getMonthlyBuildingDepreciation(property) {
     return property.financing_and_income_assumption.building_cost / 27.5 / 12;
   }
 
-  function get_annual_interest_on_loan(property) {
-    var yearly_principal_reductions = cashFlowService.getAnnualCumPrincs(property);
-    var yearly_debt_service = cashFlowService.getAnnualDebtService(property);
+  function getAnnualInterestOnLoan(property) {
+    var annualPrincipalReductions = cashFlowService.getAnnualCumPrincs(property);
+    var annualDebtService = cashFlowService.getAnnualDebtService(property);
 
-    return _.map(yearly_principal_reductions, function(reduction) {
-      return yearly_debt_service - reduction;
+    return _.map(annualPrincipalReductions, function(principalReductionThisYear) {
+      return annualDebtService - principalReductionThisYear;
     });
   }
 
-  function monthly_interest_on_loan_percentage(property) {
-    return 100 * monthly_interest_on_loan(property) / propertyService.getGrossOperatingIncome(property);
+  function getMonthlyInterestOnLoanPercentage(property) {
+    return 100 * getMonthlyInterestOnLoan(property) / propertyService.getGrossOperatingIncome(property);
   }
 
-  function monthly_interest_on_loan(property) {
+  function getMonthlyInterestOnLoan(property) {
     return propertyService.getMonthlyLoanPayment(property) - cashFlowService.getMonthlyCumPrinc(property);
   }
 
-  function net_monthly_income(property, expenses) {
-    var gross_income = propertyService.getGrossOperatingIncome(property);
-    var total_monthly_expenses = expenses.total.monthly_cost;
-    return gross_income - total_monthly_expenses;
+  function getNetMonthlyIncome(property, expenses) {
+    var grossOperatingIncome = propertyService.getGrossOperatingIncome(property);
+    var totalMonthlyExpenses = expenses.total.monthly_cost;
+    return grossOperatingIncome - totalMonthlyExpenses;
   }
 
-  function net_annual_incomes(property, expenses) {
+  function getNetAnnualIncomes(property, expenses) {
     var incomes = propertyService.getProjectedAnnualGrossOperatingIncomes(property);
     return _.map(incomes, function(income, index) {
       return income - expenses.total.yearly_costs[index];
     });
   }
 
-  function monthly_taxes(property, expenses) {
-    var tax_rate = property.operating_expenses_assumption.income_tax_rate / 100;
-    var income_before_taxes = monthly_net_income_before_taxes(property, expenses);
+  function getMonthlyTaxes(property, expenses) {
+    var taxRate = property.operating_expenses_assumption.income_tax_rate / 100;
+    var netMonthlyIncomeBeforeTaxes = getMonthlyNetIncomeBeforeTaxes(property, expenses);
 
-    return tax_rate * income_before_taxes;
+    return taxRate * netMonthlyIncomeBeforeTaxes;
   }
 
-  function annual_taxes(property, expenses) {
-    var tax_rate = property.operating_expenses_assumption.income_tax_rate / 100;
-    var income_before_taxes = annual_net_income_before_taxes(property, expenses);
+  function getAnnualTaxes(property, expenses) {
+    var taxRate = property.operating_expenses_assumption.income_tax_rate / 100;
+    var annualNetIncomeBeforeTaxes = getAnnualNetIncomeBeforeTaxes(property, expenses);
 
-    return _.map(income_before_taxes, function(income) {
-      return tax_rate * income;
+    return _.map(annualNetIncomeBeforeTaxes, function(netIncomeBeforeTaxesThisYear) {
+      return taxRate * netIncomeBeforeTaxesThisYear;
     });
   }
 });
