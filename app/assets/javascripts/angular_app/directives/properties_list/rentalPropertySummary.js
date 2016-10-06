e493fa01a116cf020e57c5336b9161d416e45846
Mikey McCormack <mccormack@berkeley.edu>
2016-10-06 16:56:25 -0700

    Camelcase everything in the rental property summary directive

--- app/assets/javascripts/angular_app/directives/properties_list/rentalPropertySummary.js
+++ app/assets/javascripts/angular_app/directives/properties_list/rentalPropertySummary.js
@@ -13,9 +13,9 @@ angular.module('rentals').directive('rentalPropertySummary', function($state, ke
           return;
         }
 
-        $scope.cap_rate = keyRentRatiosService.getCapitalizationRate($scope.property);
-        $scope.total_cost = propertyService.getTotalCost($scope.property);
-        $scope.year_one_cash_on_cash_roi = roiService.getCashOnCashReturn($scope.property)[0];
+        $scope.capRate = keyRentRatiosService.getCapitalizationRate($scope.property);
+        $scope.totalCost = propertyService.getTotalCost($scope.property);
+        $scope.yearOneCashOnCashRoi = roiService.getCashOnCashReturn($scope.property)[0];
       });
 
       $scope.goToProperty = function(id) {
