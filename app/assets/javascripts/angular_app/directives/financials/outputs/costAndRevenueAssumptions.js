7e0dcdb201dbd7cc2705c662489186caf41c2f2d
Mikey McCormack <mccormack@berkeley.edu>
2016-10-06 15:21:01 -0700

    Inline the propertyService references in the costAndRevenueAssumptions directive

--- app/assets/javascripts/angular_app/directives/financials/outputs/costAndRevenueAssumptions.js
+++ app/assets/javascripts/angular_app/directives/financials/outputs/costAndRevenueAssumptions.js
@@ -3,16 +3,15 @@ angular.module('rentals').directive('costAndRevenueAssumptions', function(proper
     templateUrl: 'investment_properties_pages/financials/outputs/costAndRevenueAssumptions.html',
     restrict: 'E',
     link: function($scope) {
-      var s = propertyService;
       $scope.$watch('rental_property', function() {
         if(!$scope.rental_property) {
           return;
         }
         if ($scope.rental_property.closing_cost && $scope.rental_property.financing_and_income_assumption) {
-          $scope.total_closing_costs = s.getClosingCosts($scope.rental_property);
-          $scope.gross_monthly_rent = s.getGrossMonthlyRent($scope.rental_property);
-          $scope.total_cost = s.getTotalCost($scope.rental_property);
-          $scope.total_gross_monthly_income = s.getTotalGrossMonthlyIncome($scope.rental_property);
+          $scope.total_closing_costs = propertyService.getClosingCosts($scope.rental_property);
+          $scope.gross_monthly_rent = propertyService.getGrossMonthlyRent($scope.rental_property);
+          $scope.total_cost = propertyService.getTotalCost($scope.rental_property);
+          $scope.total_gross_monthly_income = propertyService.getTotalGrossMonthlyIncome($scope.rental_property);
         }
       }, true);
     }
