edc68f918c5895a3c81e1361e050e615040bb63a
Mikey McCormack <mccormack@berkeley.edu>
2016-10-06 15:47:44 -0700

    Camelcase everything in the keyRentRatios directive

--- app/assets/javascripts/angular_app/directives/financials/outputs/keyRentRatios.js
+++ app/assets/javascripts/angular_app/directives/financials/outputs/keyRentRatios.js
@@ -7,14 +7,14 @@ angular.module('rentals').directive('keyRentRatios', function(propertyService, k
         if(!$scope.rental_property) {
           return;
         }
-        $scope.avg_sq_ft_per_unit = propertyService.getAverageAreaPerUnit($scope.rental_property);
-        $scope.avg_rent_per_sq_ft = propertyService.getAverageRentPerSquareFoot($scope.rental_property);
-        $scope.total_cost_per_sq_ft = propertyService.getTotalCostPerSquareFoot($scope.rental_property);
-        $scope.cost_per_unit = propertyService.getCostPerUnit($scope.rental_property);
-        $scope.cap_rate = keyRentRatiosService.getCapitalizationRate($scope.rental_property);
-        $scope.gross_rent_multiplier = keyRentRatiosService.getGrossRentMultiplier($scope.rental_property);
-        $scope.operating_efficiency = keyRentRatiosService.getOperatingEfficiency($scope.rental_property);
-        $scope.expenses_per_unit = keyRentRatiosService.getExpensesPerUnit($scope.rental_property);
+        $scope.averageSquareFootagePerUnit = propertyService.getAverageAreaPerUnit($scope.rental_property);
+        $scope.averageRentPerSquareFoot = propertyService.getAverageRentPerSquareFoot($scope.rental_property);
+        $scope.totalCostPerSquareFoot = propertyService.getTotalCostPerSquareFoot($scope.rental_property);
+        $scope.costPerUnit = propertyService.getCostPerUnit($scope.rental_property);
+        $scope.capitalizationRate = keyRentRatiosService.getCapitalizationRate($scope.rental_property);
+        $scope.grossRentMultiplier = keyRentRatiosService.getGrossRentMultiplier($scope.rental_property);
+        $scope.operatingEfficiency = keyRentRatiosService.getOperatingEfficiency($scope.rental_property);
+        $scope.expensesPerUnit = keyRentRatiosService.getExpensesPerUnit($scope.rental_property);
 
       }, true);
     }
