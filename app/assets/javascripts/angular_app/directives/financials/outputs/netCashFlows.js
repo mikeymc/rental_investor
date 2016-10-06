6c2cdf74b8199456109d8fa76cb82001fe024e79
Mikey McCormack <mccormack@berkeley.edu>
2016-10-06 09:40:46 -0700

    Camel case the internals of the net cash flows directive

--- app/assets/javascripts/angular_app/directives/financials/outputs/netCashFlows.js
+++ app/assets/javascripts/angular_app/directives/financials/outputs/netCashFlows.js
@@ -10,18 +10,18 @@ angular.module('rentals').directive('netCashFlows', function(propertyService, ca
 
         $scope.monthly_cash_flow = -1 * propertyService.getDownPayment($scope.rental_property);
         $scope.one_year_exit_net = cashFlowService.getOneYearExitNet($scope.rental_property);
-        $scope.three_year_exit_nets = three_year_exit_nets($scope.rental_property);
-        $scope.five_year_exit_nets = five_year_exit_nets($scope.rental_property);
+        $scope.three_year_exit_nets = threeYearExitNets($scope.rental_property);
+        $scope.five_year_exit_nets = fiveYearExitNets($scope.rental_property);
       }, true);
 
       /* --- Private --- */
 
-      function three_year_exit_nets(property) {
+      function threeYearExitNets(property) {
         var gain_on_sale = exitScenariosService.thirdYearGainOnSale(property);
         return cashFlowService.getThreeYearExitNets(property, gain_on_sale);
       }
 
-      function five_year_exit_nets(property) {
+      function fiveYearExitNets(property) {
         var gain_on_sale = exitScenariosService.fifthYearGainOnSale(property);
         return cashFlowService.getFiveYearExitNets(property, gain_on_sale);
       }
