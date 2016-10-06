1484b9e5e0007ba1d8a159ea66f4182038aca447
Mikey McCormack <mccormack@berkeley.edu>
2016-10-06 16:48:48 -0700

    Camelcase everything in the operatingRevenues directive

--- app/assets/javascripts/angular_app/directives/financials/outputs/operatingRevenues.js
+++ app/assets/javascripts/angular_app/directives/financials/outputs/operatingRevenues.js
@@ -7,16 +7,16 @@ angular.module('rentals').directive('operatingRevenues', function(propertyServic
         if (!$scope.rental_property) {
           return;
         }
-        $scope.monthly_gross_scheduled_rent_income = propertyService.getGrossMonthlyRent($scope.rental_property);
+        $scope.monthlyGrossScheduledRentIncome = propertyService.getGrossMonthlyRent($scope.rental_property);
         $scope.vacancy = propertyService.getVacancyOperatingExpense($scope.rental_property);
-        $scope.net_rental_income = propertyService.getNetRentalIncome($scope.rental_property);
-        $scope.other_income = $scope.rental_property.financing_and_income_assumption.other_monthly_income;
-        $scope.gross_income = propertyService.getGrossOperatingIncome($scope.rental_property);
-        $scope.projected_gross_annual_rents = propertyService.getProjectedGrossAnnualRents($scope.rental_property);
-        $scope.projected_annual_vacancy_costs = propertyService.getProjectedAnnualVacancyCosts($scope.rental_property);
-        $scope.projected_annual_net_rental_incomes = propertyService.getProjectedAnnualNetRentalIncomes($scope.rental_property);
-        $scope.projected_other_incomes = propertyService.getProjectedOtherIncomes($scope.rental_property);
-        $scope.projected_gross_incomes = propertyService.getProjectedAnnualGrossOperatingIncomes($scope.rental_property);
+        $scope.netRentalIncome = propertyService.getNetRentalIncome($scope.rental_property);
+        $scope.otherIncome = $scope.rental_property.financing_and_income_assumption.other_monthly_income;
+        $scope.grossIncome = propertyService.getGrossOperatingIncome($scope.rental_property);
+        $scope.projectedGrossAnnualRents = propertyService.getProjectedGrossAnnualRents($scope.rental_property);
+        $scope.projectedAnnualVacancyCosts = propertyService.getProjectedAnnualVacancyCosts($scope.rental_property);
+        $scope.projectedAnnualNetRentalIncomes = propertyService.getProjectedAnnualNetRentalIncomes($scope.rental_property);
+        $scope.projectedOtherIncomes = propertyService.getProjectedOtherIncomes($scope.rental_property);
+        $scope.projectedGrossIncomes = propertyService.getProjectedAnnualGrossOperatingIncomes($scope.rental_property);
       }, true);
     }
   }
