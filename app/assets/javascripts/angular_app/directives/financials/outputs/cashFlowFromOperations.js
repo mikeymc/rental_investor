0ccd0495403b2ee4af13b5ad3c3e106dc943e076
Mikey McCormack <mccormack@berkeley.edu>
2016-10-06 11:06:54 -0700

    Make cash flow view use cameCase model bindings

--- app/assets/javascripts/angular_app/directives/financials/outputs/cashFlowFromOperations.js
+++ app/assets/javascripts/angular_app/directives/financials/outputs/cashFlowFromOperations.js
@@ -10,61 +10,61 @@ angular.module('rentals').directive('cashFlowFromOperations', function(propertyS
 
         var expenses = operatingExpensesService.getAllOperatingExpenses($scope.rental_property);
 
-        $scope.monthly_debt_service = propertyService.getMonthlyLoanPayment($scope.rental_property);
-        $scope.annual_debt_service = cashFlowService.getAnnualDebtService($scope.rental_property);
-        $scope.cash_available = propertyService.getNetOperatingIncome($scope.rental_property, expenses);
-        $scope.annual_cash_available = propertyService.getNetAnnualOperatingIncomes($scope.rental_property, expenses);
-        $scope.monthly_cf_remaining = cashFlowService.getMonthlyCashFlowRemaining($scope.rental_property);
-        $scope.annual_cfs_remaining = cashFlowService.getAnnualCashFlowsRemaining($scope.rental_property);
-        $scope.monthly_principal_reduction = cashFlowService.getMonthlyCumPrinc($scope.rental_property);
-        $scope.yearly_principal_reductions = cashFlowService.getAnnualCumPrincs($scope.rental_property);
-        $scope.monthly_total_return = cashFlowService.getMonthlyTotalReturn($scope.rental_property);
-        $scope.annual_total_returns = cashFlowService.getAnnualTotalReturns($scope.rental_property);
-        $scope.monthly_cf_debt_servicing_ratio = monthly_cf_debt_servicing_ratio($scope.rental_property);
-        $scope.annual_cf_debt_servicing_ratio = annual_cf_debt_servicing_ratio($scope.rental_property);
-        $scope.monthly_net_income_after_taxes = noiService.getMonthlyNetIncomeAfterTaxes($scope.rental_property, expenses);
-        $scope.annual_net_income_after_taxes = noiService.getAnnualNetIncomeAfterTaxes($scope.rental_property, expenses);
-        $scope.monthly_building_depreciation = noiService.getMonthlyBuildingDepreciation($scope.rental_property);
-        $scope.annual_building_depreciation = noiService.getAnnualBuildingDepreciation($scope.rental_property);
-        $scope.monthly_interest_on_loan = noiService.getMonthlyInterestOnLoan($scope.rental_property);
-        $scope.annual_interest_on_loan = noiService.getAnnualInterestOnLoan($scope.rental_property);
-        $scope.monthly_total_cf_from_operations = monthly_total_cf_from_operations($scope.rental_property, expenses);
-        $scope.annual_total_cf_from_operations = annual_total_cf_from_operations($scope.rental_property, expenses);
+        $scope.monthlyDebtService = propertyService.getMonthlyLoanPayment($scope.rental_property);
+        $scope.annualDebtService = cashFlowService.getAnnualDebtService($scope.rental_property);
+        $scope.cashAvailable = propertyService.getNetOperatingIncome($scope.rental_property, expenses);
+        $scope.annualCashAvailable = propertyService.getNetAnnualOperatingIncomes($scope.rental_property, expenses);
+        $scope.monthlyCashFlowRemaining = cashFlowService.getMonthlyCashFlowRemaining($scope.rental_property);
+        $scope.annualCashFlowsRemaining = cashFlowService.getAnnualCashFlowsRemaining($scope.rental_property);
+        $scope.monthlyPrincipalReduction = cashFlowService.getMonthlyCumPrinc($scope.rental_property);
+        $scope.yearlyPrincipalReductions = cashFlowService.getAnnualCumPrincs($scope.rental_property);
+        $scope.monthlyTotalReturn = cashFlowService.getMonthlyTotalReturn($scope.rental_property);
+        $scope.annualTotalReturns = cashFlowService.getAnnualTotalReturns($scope.rental_property);
+        $scope.monthlyCashFlowDebtServicingRatio = monthlyCashFlowDebtServicingRatio($scope.rental_property);
+        $scope.annualCashFlowDebtServicingRatio = annualCashFlowDebtServicingRatio($scope.rental_property);
+        $scope.monthlyNetIncomeAfterTaxes = noiService.getMonthlyNetIncomeAfterTaxes($scope.rental_property, expenses);
+        $scope.annualNetIncomeAfterTaxes = noiService.getAnnualNetIncomeAfterTaxes($scope.rental_property, expenses);
+        $scope.monthlyBuildingDepreciation = noiService.getMonthlyBuildingDepreciation($scope.rental_property);
+        $scope.annualBuildingDepreciation = noiService.getAnnualBuildingDepreciation($scope.rental_property);
+        $scope.monthlyInterestOnLoan = noiService.getMonthlyInterestOnLoan($scope.rental_property);
+        $scope.annualInterestOnLoan = noiService.getAnnualInterestOnLoan($scope.rental_property);
+        $scope.monthlyTotalCashFlowFromOperations = monthlyTotalCashFlowFromOperations($scope.rental_property, expenses);
+        $scope.annualTotalCashFlowFromOperations = annualTotalCashFlowFromOperations($scope.rental_property, expenses);
 
         /* --- Private --- */
 
-        function monthly_total_cf_from_operations(property, expenses) {
+        function monthlyTotalCashFlowFromOperations(property, expenses) {
           var depreciation = noiService.getMonthlyBuildingDepreciation(property);
-          var income_after_taxes = noiService.getMonthlyNetIncomeAfterTaxes(property, expenses);
+          var incomeAfterTaxes = noiService.getMonthlyNetIncomeAfterTaxes(property, expenses);
 
-          return depreciation + income_after_taxes;
+          return depreciation + incomeAfterTaxes;
         }
 
-        function annual_total_cf_from_operations(property, expenses) {
+        function annualTotalCashFlowFromOperations(property, expenses) {
           var depreciation = noiService.getAnnualBuildingDepreciation(property);
-          var income_after_taxes = noiService.getAnnualNetIncomeAfterTaxes(property, expenses);
+          var incomeAfterTaxes = noiService.getAnnualNetIncomeAfterTaxes(property, expenses);
 
-          return _.map(income_after_taxes, function(income) {
+          return _.map(incomeAfterTaxes, function(income) {
             return depreciation + income;
           });
         }
 
-        function annual_cf_debt_servicing_ratio(property) {
+        function annualCashFlowDebtServicingRatio(property) {
           var expenses = operatingExpensesService.getAllOperatingExpenses(property);
-          var annual_cash_available = propertyService.getNetAnnualOperatingIncomes(property, expenses);
-          var annual_debt_service = cashFlowService.getAnnualDebtService(property);
+          var annualCashAvailable = propertyService.getNetAnnualOperatingIncomes(property, expenses);
+          var annualDebtService = cashFlowService.getAnnualDebtService(property);
 
-          return _.map(annual_cash_available, function(cash) {
-            return 100 * cash / annual_debt_service;
+          return _.map(annualCashAvailable, function(cash) {
+            return 100 * cash / annualDebtService;
           });
         }
 
-        function monthly_cf_debt_servicing_ratio(property) {
+        function monthlyCashFlowDebtServicingRatio(property) {
           var expenses = operatingExpensesService.getAllOperatingExpenses(property);
-          var monthly_debt_service = propertyService.getMonthlyLoanPayment(property);
-          var cash_available = propertyService.getNetOperatingIncome(property, expenses);
+          var monthlyDebtService = propertyService.getMonthlyLoanPayment(property);
+          var cashAvailable = propertyService.getNetOperatingIncome(property, expenses);
 
-          return 100 * cash_available / monthly_debt_service;
+          return 100 * cashAvailable / monthlyDebtService;
         }
       }, true);
     }
